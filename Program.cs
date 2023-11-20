71ccf71f036f063626e9f39c87a729e9a5e5fc63
Akorolev1-d <71078667+Akorolev1-d@users.noreply.github.com>
2023-11-20 20:18:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using MathS;
+using System.Runtime.Intrinsics.Arm;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Test App");
+        double a = 2;
+        double b = 5;
+        double c = 4;
+        double radius = 11;
+        SquareCircle S = new(radius);
+        SquareTriangle Triangle = new(a, b, c);
+        S.Square();
+        Triangle.Square();
+    }
+}
